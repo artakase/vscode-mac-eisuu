bdbb96ab3bc4cd9ec8c0e2ad57fc54c8548a904a
TAKASE Arihiro <artakase@gmail.com>
2025-02-26 17:11:40 +0900

    update config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,4 @@
 module.exports = {
     singleQuote: true,
     printWidth: 120,
-    tabWidth: 4,
-    endOfLine: 'auto',
-    trailingComma: 'none',
-    overrides: [
-        {
-            files: ['*.yml', '*.yaml'],
-            options: {
-                tabWidth: 2
-            }
-        }
-    ]
 };
